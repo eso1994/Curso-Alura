0d62483857db994aa4894ab3e3c8c4b52aef80d9
eso1994 <93888096+eso1994@users.noreply.github.com>
2021-11-26 18:08:10 -0300

    cetaloko

---
+++ Curso Alura/testeConcat.js
@@ -0,0 +1,6 @@
+const turma1 = ['ana', 'mariana', 'juliana', 'luciana', 'diana']
+const turma2 = ['samara', 'lucimara', 'mara', 'tamara', 'mayara']
+
+const turmaUnificada = turma1.concat(turma2)
+
+console.log(turmaUnificada)
