37277a4e1eeba7633df01e875c356c4fdd9280d2
eso1994 <93888096+eso1994@users.noreply.github.com>
2021-11-26 18:47:41 -0300

    toQuaseBom
    mentira, pra ser ruim ainda falta muito

---
+++ Curso Alura/listaDeListas.js
@@ -0,0 +1,8 @@
+var alunos = ['ana', 'andré', 'joao', 'anne', 'almir']
+var media = [10, 9, 6, 5, 3]
+
+var mediaDosAlunos = [alunos, media]
+
+for (let key = 0; key < alunos.length; key++){
+     console.log(`${mediaDosAlunos[0] [key]}, sua média é ${mediaDosAlunos[1] [key]}`)
+}
