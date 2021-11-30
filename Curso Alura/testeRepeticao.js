d3403cdb471605062f84c48898492358f0cdbc88
eso1994 <93888096+eso1994@users.noreply.github.com>
2021-11-29 23:31:38 -0300

    moreTest

--- Curso Alura/testeRepeticao.js
+++ Curso Alura/testeRepeticao.js
@@ -1,5 +1,18 @@
+//show the array's list and yours index
 let list = [100, 200, 300, 400, 500, 600]
 
 for (let i = 0; i < list.length; i++) {
     console.log(i, list[i])
 }
+//add elements value and divide by number of list elements
+
+var schoolGrades = [6, 8.5, 8, 9.5]
+var media = 0;
+
+for (let i = 0; i < schoolGrades.length; i++){
+    media += schoolGrades[i];
+}
+
+media /= schoolGrades.length;
+
+console.log(media)
