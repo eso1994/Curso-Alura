d84562ee5a507b078fa48f9aa469693876bf3330
eso1994 <93888096+eso1994@users.noreply.github.com>
2021-12-01 23:40:31 -0300

    addFilds

---
+++ JS-curso-3/addInformations.js
@@ -0,0 +1,16 @@
+const client = {
+    name: 'José',
+    age: 40,
+    cpf: '22345789021',
+    email: 'pai_de_jesus@email.com'
+}
+
+console.log(client)
+
+client.phone = '04792271266666666'
+
+console.log(client)
+
+client.name = 'Maria José'
+
+console.log(client)
