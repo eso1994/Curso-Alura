ce5733dd0f0a9a089eaab4cba006237412a98298
eso1994 <93888096+eso1994@users.noreply.github.com>
2021-12-05 00:51:49 -0300

    jsonCONVERTION

---
+++ JS-curso-3/JSONconvertion.js
@@ -0,0 +1,13 @@
+const jsonLivro = JSON.stringify({
+    id: 50,
+    titulo: "Primeiros Passos com NodeJS",
+    autor: "João Rubens",
+    categoria: "programação",
+    versoes: ["ebook", "impresso"]
+   })
+   
+    console.log(jsonLivro)
+
+const objLivro = JSON.parse(jsonLivro)
+
+    console.log(objLivro)
