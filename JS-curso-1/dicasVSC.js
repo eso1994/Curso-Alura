1d85a311f1f63db5a19f0075c3166bf30999a335
eso1994 <93888096+eso1994@users.noreply.github.com>
2021-12-07 22:49:03 -0300

    Create dicasVSC.js

---
+++ JS-curso-1/dicasVSC.js
@@ -0,0 +1,16 @@
+let teste = ["olá, mundo", "Hi, world"];
+
+for (let i = 0; i < teste.length; i++) {
+    console.log(teste[i])
+
+}
+// usando o atalho ctrl + shift + L, consigo selecionar todas as ocorrencias daquela instancia! Ao selecionar a variavel teste e usar o atalho, todas as "teste" serão selecionadas tambem.
+
+// Já quando quisermos renomear uma ocorrencia que ja existe e ja tem dependencias, podemos usar a tecla F2 para que todos os lugares onde essa ocorrencia estiver determinada também seja atualizada com o novo nome
+
+// var a = 1;
+// var b = 2;
+// //var c = 3;
+// var d = 4;
+
+// usando o atalho ctrl + ; transformamos todas as linhas selecionadas em comentários. Para defazer basta repetir o processo.
